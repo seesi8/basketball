6f0d804f7571977f797ef766cd6f3696bc1c8da2
Samuel Liebert <ballbertllc@gmail.com>
2024-12-02 16:05:37 -0600

    mobile

--- dynasty_basketball/src/app/layout.jsx
+++ dynasty_basketball/src/app/layout.jsx
@@ -1,24 +1,27 @@
-"use client"
+"use client";
 
 import localFont from "next/font/local";
 import "./styles/globals.css";
-import Header from "./components/header"
-import { Roboto_Flex } from 'next/font/google'
+import Header from "./components/header";
+import { Roboto_Flex } from "next/font/google";
 import Sidebar from "./components/sidebar";
 
-const roboto = Roboto_Flex({ subsets: ['latin'], weight: "variable" })
+const roboto = Roboto_Flex({ subsets: ["latin"], weight: "variable" });
 
 export default function RootLayout({ children }) {
-
-  return (
-    <html lang="en">
-      <body className={`${roboto.className}`}>
-        <div style={{positon: "absolute"}}>
-          <Header/>
-          <Sidebar/>
-        </div>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <body className={`${roboto.className}`}>
+                <div style={{ positon: "absolute" }}>
+                    <Header />
+                    <Sidebar />
+                </div>
+                {children}
+            </body>
+        </html>
+    );
 }
